aca919876fe367dd497f3f4fc8036f290ac924e8
SuperPsycho4347 <superpsycho4347@gmail.com>
2024-11-26 22:08:32 +0500

    added initial styling and logic.

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-const myLibrary = [];
+const myLibrary = [
+    {title: "War And Peace", author: "Leo Tolstoy", pages: "1,225", status: "read"}
+];
 
-function Book() {
+// const container = document.querySelector('container');
+const books = document.querySelector('.books');
+const bookContainer = document.createElement('div');
+bookContainer.classList.add('book');
+const bookTitle = document.createElement('h2');
+bookTitle.textContent = myLibrary[0].title;
+bookContainer.appendChild(bookTitle);
+const bookAuthor = document.createElement('p');
+bookAuthor.textContent = myLibrary[0].author;
+bookContainer.appendChild(bookAuthor);
+const bookPages = document.createElement('p');
+bookPages.textContent = myLibrary[0].pages;
+bookContainer.appendChild(bookPages);
+const bookStatus =document.createElement('p');
+bookStatus.textContent = myLibrary[0].status;
+bookContainer.appendChild(bookStatus);
+books.appendChild(bookContainer);
 
+function Book(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
 }
 
 function addBook() {
