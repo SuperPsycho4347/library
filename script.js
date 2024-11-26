a76966a93c89f6ed435cbf8305ae128ee365fb78
SuperPsycho4347 <superpsycho4347@gmail.com>
2024-11-27 01:21:48 +0500

    added working logic.

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function Book(title, author, pages, status) {
 
 function createBookCard(newBook , bookIndex) {
     const bookCard = document.createElement('div');
+    bookCard.classList.add('book');
     const bookTitle = document.createElement('h2');
     bookTitle.classList.add('book-title');
     const bookAuthor = document.createElement('p');
@@ -24,7 +25,7 @@ function createBookCard(newBook , bookIndex) {
     const bookStatus = document.createElement('p')
     bookStatus.classList.add('book-status')
 
-    bookTitle.textContent = `${newBook.title};`
+    bookTitle.textContent = `${newBook.title}`
     bookAuthor.textContent = `${newBook.author}`;
     bookPages.textContent = `${newBook.pages}`;
     bookStatus.textContent = `${newBook.status}`;
@@ -34,6 +35,19 @@ function createBookCard(newBook , bookIndex) {
     bookCard.appendChild(bookPages);
     bookCard.appendChild(bookStatus);
 
+    const delBtn = document.createElement('button')
+    delBtn.textContent = "Delete"
+    delBtn.classList.add('delete');
+    const toggleBtn = document.createElement('button');
+    toggleBtn.textContent = "Toggle"
+    toggleBtn.classList.add('toggle');
+
+    
+    delBtn.addEventListener('click', () => deleteBook(bookIndex));
+    toggleBtn.addEventListener('click', () => toggleStatus(bookIndex, bookStatus));
+    
+    bookCard.appendChild(delBtn);
+    bookCard.appendChild(toggleBtn);
     return bookCard;
 }
 
@@ -56,27 +70,37 @@ function createLibrary() {
     bodyContainer.appendChild(baseContainer);
 }
 
+function refreshLibrary() {
+    const bookContainer = document.querySelector('.books-container');
+    if (bookContainer) {
+        bookContainer.remove();
+    }
+    createLibrary();
+}
+
+function deleteBook(bookIndex) {
+    myLibrary.splice(bookIndex, 1);
+    refreshLibrary();
+}
+
+function toggleStatus(bookIndex, bookStatus) {
+    const myBook = myLibrary[bookIndex];
+    if (myBook.status === 'not read') {
+        myBook.status = 'reading';
+    }
+    else if (myBook.status === 'reading') {
+        myBook.status = 'read';
+    }
+    else {
+        myBook.status = 'not read';
+    }
+
+    bookStatus.textContent = myBook.status;
+}
+
 // Execution
 const myLibrary = [
 ];
-
-// const books = document.querySelector('.books');
-// const bookContainer = document.createElement('div');
-// bookContainer.classList.add('book');
-// const bookTitle = document.createElement('h2');
-// bookTitle.textContent = myLibrary[0].title;
-// bookContainer.appendChild(bookTitle);
-// const bookAuthor = document.createElement('p');
-// bookAuthor.textContent = myLibrary[0].author;
-// bookContainer.appendChild(bookAuthor);
-// const bookPages = document.createElement('p');
-// bookPages.textContent = myLibrary[0].pages;
-// bookContainer.appendChild(bookPages);
-// const bookStatus =document.createElement('p');
-// bookStatus.textContent = myLibrary[0].status;
-// bookContainer.appendChild(bookStatus);
-// books.appendChild(bookContainer);
-
 
 createLibrary();
 
@@ -99,7 +123,6 @@ submitBtn.addEventListener('click', (event) => {
 
     const newBook = new Book(titleInput, authorInput, pagesInput, statusInput);
 
-    console.log('hell')
     myLibrary.push(newBook);
 
     addBook(newBook);
