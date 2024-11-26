4ad1102791380484ca03bf40e7258872ea1984e3
SuperPsycho4347 <superpsycho4347@gmail.com>
2024-11-26 23:47:40 +0500

    HTML: adjusted structure

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
-const myLibrary = [
-    {title: "War And Peace", author: "Leo Tolstoy", pages: "1,225", status: "read"}
-];
+// Variables
+const bodyContainer = document.querySelector('.container')
+const addBookDialog = document.querySelector('.add-book-dialog');
+const addBookBtn = document.querySelector('#add-book')
+const submitBtn = document.querySelector('.submit');
+const cancelBtn = document.querySelector('.cancel');
 
-const books = document.querySelector('.books');
-const bookContainer = document.createElement('div');
-bookContainer.classList.add('book');
-const bookTitle = document.createElement('h2');
-bookTitle.textContent = myLibrary[0].title;
-bookContainer.appendChild(bookTitle);
-const bookAuthor = document.createElement('p');
-bookAuthor.textContent = myLibrary[0].author;
-bookContainer.appendChild(bookAuthor);
-const bookPages = document.createElement('p');
-bookPages.textContent = myLibrary[0].pages;
-bookContainer.appendChild(bookPages);
-const bookStatus =document.createElement('p');
-bookStatus.textContent = myLibrary[0].status;
-bookContainer.appendChild(bookStatus);
-books.appendChild(bookContainer);
-
+// Functions
 function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
@@ -29,3 +16,48 @@ function Book(title, author, pages, status) {
 function addBook() {
     
 }
+
+function createLibrary() {
+    const baseContainer = document.createElement('div');
+    baseContainer.classList.add('books-container');
+    bodyContainer.appendChild(baseContainer);
+}
+
+// Execution
+const myLibrary = [
+    {title: "War And Peace", author: "Leo Tolstoy", pages: "1,225", status: "read"}
+];
+
+// const books = document.querySelector('.books');
+// const bookContainer = document.createElement('div');
+// bookContainer.classList.add('book');
+// const bookTitle = document.createElement('h2');
+// bookTitle.textContent = myLibrary[0].title;
+// bookContainer.appendChild(bookTitle);
+// const bookAuthor = document.createElement('p');
+// bookAuthor.textContent = myLibrary[0].author;
+// bookContainer.appendChild(bookAuthor);
+// const bookPages = document.createElement('p');
+// bookPages.textContent = myLibrary[0].pages;
+// bookContainer.appendChild(bookPages);
+// const bookStatus =document.createElement('p');
+// bookStatus.textContent = myLibrary[0].status;
+// bookContainer.appendChild(bookStatus);
+// books.appendChild(bookContainer);
+
+
+createLibrary();
+
+addBookBtn.addEventListener('click', () => {
+    addBookDialog.showModal();
+})
+
+cancelBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    addBookDialog.close();
+})
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    addBookDialog.close();
+})
